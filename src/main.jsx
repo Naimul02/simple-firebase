a23f270474f61cde54cbadf8631f29602dcda9d4
Naimul02 <naimulislum39@gmail.com>
2024-03-29 22:23:01 +0600

    commit no 2

--- src/main.jsx
+++ src/main.jsx
@@ -5,6 +5,7 @@ import "./index.css";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import Main from "./layout/Main.jsx";
 import Home from "./components/Home/Home.jsx";
+import Login from "./components/Login/Login.jsx";
 const router = createBrowserRouter([
   {
     path: "/",
@@ -13,6 +14,10 @@ const router = createBrowserRouter([
       {
         path: "/",
         element: <Home></Home>,
+      },
+      {
+        path: "/login",
+        element: <Login></Login>,
       },
     ],
   },
